7633b9edd916bdb7d2383a2f1699b08db0d15a41
Christophe Noël <christophe@taotesting.com>
2017-08-29 09:55:24 +0200

    added platform theme

--- manifest.php
+++ manifest.php
@@ -1,22 +1,22 @@
 <?php
-/**  
+/**
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; under version 2
  * of the License (non-upgradable).
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- * 
+ *
  * Copyright (c) 2017 (original work) Open Assessment Technologies SA;
- *               
- * 
+ *
+ *
  */
 
 /**
@@ -27,7 +27,7 @@ return array(
     'label' => 'trainingBranding',
     'description' => '',
     'license' => 'GPL-2.0',
-    'version' => '0.1.0',
+    'version' => '0.2.0',
     'author' => 'Open Assessment Technologies SA',
     'requires' => array(
         'tao' => '>=12.13.2'
@@ -37,16 +37,18 @@ return array(
         array('grant', 'http://www.tao.lu/Ontologies/generis.rdf#trainingBrandingManager', array('ext'=>'trainingBranding')),
     ),
     'install' => array(
+        oat\trainingBranding\scripts\install\SetPlatformTheme::class
     ),
     'uninstall' => array(
     ),
+    'update' => \oat\trainingBranding\scripts\update\Updater::class,
     'routes' => array(
         '/trainingBranding' => 'oat\\trainingBranding\\controller'
-    ),    
+    ),
     'constants' => array(
         # views directory
         "DIR_VIEWS" => dirname(__FILE__).DIRECTORY_SEPARATOR."views".DIRECTORY_SEPARATOR,
-        
+
         #BASE URL (usually the domain root)
         'BASE_URL' => ROOT_URL.'trainingBranding/',
     ),
